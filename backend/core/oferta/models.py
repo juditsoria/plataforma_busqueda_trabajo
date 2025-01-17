888f0412d1d3b58eed23388a6ae8848750bee180
MatiasAFare <matiasfare@gmail.com>
2025-01-17 18:36:06 -0300

    Fix: Requirements changed

--- backend/core/oferta/models.py
+++ backend/core/oferta/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from core.reclutador.models import Reclutador
 
-# Create your models here.
+# # Create your models here.
+# class Oferta(models.model):
+#     id_oferta = models.AutoField(primary_key = True)
+#     descripcion = models.TextField(null = False)
+#     fecha_publicacion = models.DataField(auto_now_add = True)
+#     salario = models.IntegerField(null = True, blank = True)
+#     ubicacion = models.CharField(max_length = 100)
+#     id_reclutador = models.ForeignKey('Reclutador', on_delete = models.CASCADE)
+    
