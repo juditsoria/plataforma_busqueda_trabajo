f1bcc8cd3fac353882c987d1a0016bec76a08794
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-01-22 11:02:25 -0500

    feat: update TypeScript paths and Vite aliases for improved module resolution

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+    },
+  },
 })
