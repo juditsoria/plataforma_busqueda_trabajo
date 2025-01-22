0c69fed2c849b07d95a9b3bdba21f5c75274d54e
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-01-22 11:11:21 -0500

    feat: refactor import paths for components and update Header layout

--- frontend/src/components/_shared/Header.tsx
+++ frontend/src/components/_shared/Header.tsx
@@ -1,15 +1,27 @@
-import { Button } from '../ui/button'
+import { Button } from '@components/ui/button'
+import { Link } from 'react-router-dom'
 
 export function Header () {
   return (
-    <header className="fixed p-5  top-0 left-0 right-0 bg-slate-400">
-      <div className="mx-96 flex items-center justify-between ">
-        <h1>ReclutaLent</h1>
-        <div className="flex items-center justify-center gap-4">
-          <Button>Inicio</Button>
-          <Button>Empleos</Button>
-          <Button>Registrarse</Button>
-        </div>
+    <header className="fixed p-4 top-0 left-0 right-0 bg-accent">
+      <div className="mx-52 flex items-center justify-between">
+        <Link to="/">
+          <img src="/logo.webp" alt="Logo ReclutaLent" className='w-[120px] object-contain'/>
+        </Link>
+        <nav className="flex items-center justify-center gap-4">
+          <ul className='list-none flex gap-4'>
+            <li>
+              <Link to="/sign-in">
+                <Button variant={'secondary'}>Iniciar Sesión</Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/sign-up">
+                <Button>Registrarse</Button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   )
