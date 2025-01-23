c5b609a25c7fce33d21467001e0e3a9f6a31f269
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-01-23 10:17:22 -0500

    feat: add custom toast hook and API configuration

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api' : 'https://reclutalent.vercel.app/api'
+})
