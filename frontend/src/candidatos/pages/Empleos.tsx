e74b84f4dc731c0c8b73db5d69bf0332899e8abf
AdrifeCova <adrife2000@hotmail.com>
2025-01-25 16:07:28 -0600

    feat: added searchBar component with his styles

--- frontend/src/candidatos/pages/Empleos.tsx
+++ frontend/src/candidatos/pages/Empleos.tsx
@@ -1,11 +1,14 @@
 import Main from '../components/_shared/main/Main'
 import Profile from '../components/_shared/profile/Profile'
+import Search from '../components/searchbar/Search'
 
 const Empleos = () => {
   return (
     <div className='max-w-[1400px] mx-auto mt-24 px-4 flex gap-6'>
       <Profile />
-      <Main />
+      <Main>
+        <Search/>
+      </Main>
     </div>
   )
 }
