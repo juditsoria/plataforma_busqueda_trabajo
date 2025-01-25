e7000058901c79661451ca0711035b9e32af49fd
AdrifeCova <adrife2000@hotmail.com>
2025-01-25 15:38:00 -0600

    feat: display navbar base of the role

--- frontend/src/components/_shared/Header.tsx
+++ frontend/src/components/_shared/Header.tsx
@@ -1,8 +1,25 @@
 import { Button } from '@components/ui/button'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import navigation from './navigation.json'
 
 export function Header () {
+  const navigate = useLocation()
+
+  const getRol = () => {
+    const role = localStorage.getItem('role')
+
+    switch (role) {
+      case 'admin':
+        return 'admin'
+      case 'candidato':
+        return 'candidato'
+      case 'reclutador':
+        return 'reclutador'
+      default:
+        return 'user'
+    }
+  }
+
   return (
     <header className="fixed py-4 top-0 left-0 right-0 bg-accent z-10">
       <div className="max-w-[1400px] px-4 mx-auto flex items-center justify-between">
@@ -12,11 +29,20 @@ export function Header () {
         <nav className="flex items-center justify-center gap-4">
           <div className='flex gap-4'>
             {
-              navigation.length > 0 && navigation.map(({ name, path, variant }, inx) => (
-              <Link key={inx} to={path}>
-                  <Button variant={variant as 'secondary' | 'default' | 'link' | 'destructive' | 'outline' | 'ghost' || 'default'}>{name}</Button>
-              </Link>
-              ))
+              getRol() === 'candidato'
+                ? <>
+                  <Link to='/'>
+                    <Button variant={navigate.pathname === '/' ? 'outline' : 'default'}>Inicio</Button>
+                  </Link>
+                  <Link to='/empleos'>
+                    <Button variant={navigate.pathname === '/empleos' ? 'secondary' : 'default'}>Empleos</Button>
+                  </Link>
+                </>
+                : navigation.map(({ name, path, variant }, inx) => (
+                <Link key={inx} to={path}>
+                    <Button variant={variant as 'secondary' | 'default' | 'link' | 'destructive' | 'outline' | 'ghost' || 'default'}>{name}</Button>
+                </Link>
+                ))
             }
           </div>
         </nav>
