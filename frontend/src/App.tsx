e8da38e0f33642ea1ce8261003a651dc52c905ff
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-01-31 11:34:41 -0500

    feat: add routes for CandidatesOfferRecruiter and CandidateRecruiter pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import { MainLayout } from '@layout/MainLayout'
 import HomePageRecruiter from '@/recruiter/pages/HomePageRecruiter'
 import { Toaster } from './components/ui/toaster'
+import CandidatesOfferRecruiter from '@/recruiter/pages/CandidatesOfferRecruiter'
+import CandidateRecruiter from './recruiter/pages/CandidateRecruiter'
 
 function App () {
   return (
@@ -10,6 +12,8 @@ function App () {
         <Route path="/" element={<MainLayout />}>
         </Route>
         <Route path="/recruiter/home" element={<HomePageRecruiter />} />
+        <Route path="/recruiter/offer/:offerId" element={<CandidatesOfferRecruiter />} />
+        <Route path="/recruiter/candidate/:candidateId" element={<CandidateRecruiter />} />
       </Routes>
       <Toaster />
     </>
