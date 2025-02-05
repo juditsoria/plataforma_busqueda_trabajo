a7e788d2a4659932498592f8bd4fda220cfbc68e
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-02-04 20:43:38 -0500

    refactor: clean up imports and improve layout in Header and Popover components; adjust styles in CandidateInfo

--- frontend/src/components/ui/popover.tsx
+++ frontend/src/components/ui/popover.tsx
@@ -1,12 +1,12 @@
-import * as Popover from '@radix-ui/react-popover';
+import * as Popover from '@radix-ui/react-popover'
 
 interface PopoverComponentProps {
-  trigger?: React.ReactNode;
-  content?: React.ReactNode;
+  trigger?: React.ReactNode
+  content?: React.ReactNode
   className?: string
 }
 
-export function PopoverComponent({ trigger, content, className }: PopoverComponentProps) {
+export function PopoverComponent ({ trigger, content, className }: PopoverComponentProps) {
   return (
     <div className={className}>
         <Popover.Root>
@@ -15,10 +15,10 @@ export function PopoverComponent({ trigger, content, className }: PopoverCompone
             {trigger}
             </Popover.Trigger>
         )}
-        <Popover.Content className="mt-1 p-2 mx-4 bg-background rounded shadow-md">
+        <Popover.Content className="p-2 mx-4 mt-1 rounded shadow-md bg-background">
             {content}
         </Popover.Content>
         </Popover.Root>
     </div>
-  );
+  )
 }
