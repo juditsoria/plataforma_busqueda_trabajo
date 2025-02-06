6411b1d2e6ece0bf0f28578e9fc2c0a3c53b6702
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-02-06 10:26:07 -0500

    feat: add custom hook for fetching candidates postulates and initial values for application

---
+++ frontend/src/recruiter/initial-values/aplicacion.ts
@@ -0,0 +1,9 @@
+import { USER_INITIAL_VALUES } from './recruiter'
+
+export const APLICACION_INITIAL_VALUES = {
+  id_aplicacion: 0,
+  candidato: USER_INITIAL_VALUES,
+  fecha_aplicacion: '',
+  estado: '',
+  id_oferta: 0
+}
