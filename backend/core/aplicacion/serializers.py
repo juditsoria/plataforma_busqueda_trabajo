6fa877593f1dd196fe198756b3e0499466e079e3
MatiasAFare <matiasfare@gmail.com>
2025-02-06 15:41:25 -0300

    Fix: renamed id_oferta as oferta in aplicacion/serializer

--- backend/core/aplicacion/serializers.py
+++ backend/core/aplicacion/serializers.py
@@ -6,7 +6,7 @@ class AplicacionSerializer(serializers.ModelSerializer):
     candidato = serializers.PrimaryKeyRelatedField(queryset=Candidato.objects.all())
     fecha_aplicacion = serializers.DateTimeField(required=False, read_only=True)  # La fecha puede ser auto asignada
     estado = serializers.CharField(required=True)
-    id_oferta = serializers.PrimaryKeyRelatedField(queryset=Oferta.objects.all(), source='id_oferta')
+    oferta = serializers.PrimaryKeyRelatedField(queryset=Oferta.objects.all(), source='id_oferta')
     
     class Meta:
         model = Aplicacion
